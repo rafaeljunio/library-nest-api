1d210f22218f1dfc7fd016a335411f307354e054
Rafael Souza <rafael.souza@iexfy.com>
2024-07-11 11:29:54 -0300

    feat: add pagination

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,15 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common'
 import { BookService } from './book.service'
 import type { Book } from './schemas/book.schema'
 import { CreateBookDto } from './dto/create-book.dto'
 import type { UpdateBookDto } from './dto/update-book.dto'
+
+import { ParsedUrlQuery } from 'node:querystring'
+
 @Controller('books')
 export class BookController {
   constructor(private bookService: BookService) {}
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAll()
+  async getAllBooks(@Query() query: ParsedUrlQuery): Promise<Book[]> {
+    return this.bookService.findAll(query)
   }
 
   @Post()
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose'
 import { Book } from './schemas/book.schema'
 import * as mongoose from 'mongoose'
 
+import { ParsedUrlQuery } from 'node:querystring'
+
 @Injectable()
 export class BookService {
   constructor(
@@ -10,8 +12,25 @@ export class BookService {
     private bookModel: mongoose.Model<Book>,
   ) {}
 
-  async findAll(): Promise<Book[]> {
-    const books = await this.bookModel.find()
+  async findAll(query: ParsedUrlQuery): Promise<Book[]> {
+    const resPerPage = 2
+    const currentPage = Number(query.page) || 1
+    const skip = resPerPage * (currentPage - 1)
+
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {}
+
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip)
+
     return books
   }
 
