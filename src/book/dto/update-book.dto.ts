bb22a664e8704940521ed680463762af4dd2fb61
Rafael Souza <rafael.souza@iexfy.com>
2024-07-11 15:29:42 -0300

    feat: validation pipes

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,9 +1,24 @@
-import type { Category } from '../schemas/book.schema'
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator'
+import { Category } from '../schemas/book.schema'
 
 export class UpdateBookDto {
+  @IsNotEmpty()
+  @IsString()
   readonly title: string
+
+  @IsNotEmpty()
+  @IsString()
   readonly description: string
+
+  @IsNotEmpty()
+  @IsString()
   readonly author: string
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly price: number
+
+  @IsNotEmpty()
+  @IsEnum(Category, { message: 'Please enter correct category.' })
   readonly category: Category
 }
